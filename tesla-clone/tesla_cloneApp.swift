b98cf06dde24e02ee6da076436f2822e98c1717b
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-09-14 09:20:02 -0400

    Initial Commit

---
+++ tesla-clone/tesla_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  tesla_cloneApp.swift
+//  tesla-clone
+//
+//  Created by Silvanei  Martins on 14/09/22.
+//
+
+import SwiftUI
+
+@main
+struct tesla_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
